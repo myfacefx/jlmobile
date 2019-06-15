d59a71251335665faa2f64c70fe29332097fd962
Ervan <ervansanjaya@gmail.com>
2019-06-15 21:09:12 +0700

    Update AndroidX, Login with FB, Login Static UI

--- lib/pages/user/login.dart
+++ lib/pages/user/login.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:jlf_mobile/globals.dart' as globals;
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+
 
 class LoginPage extends StatefulWidget {
   @override
@@ -10,6 +12,13 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPage extends State<LoginPage> {
+  FacebookLogin facebookLogin = FacebookLogin();
+  
+  FocusNode focusNode = FocusNode();
+  
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
   Widget _floatingButton(String icon) {
     Color backgr =
         icon == "Facebook" ? Color.fromRGBO(45, 80, 155, 1) : Colors.white;
@@ -20,7 +29,28 @@ class _LoginPage extends State<LoginPage> {
       padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
       child: FloatingActionButton.extended(
         onPressed: () {
-          Navigator.pushNamed(context, "/home");
+          facebookLogin.logInWithReadPermissions(['email'])
+          .then((result) {
+            print("TOKEN: " + result.accessToken.token);   
+
+            switch (result.status) {
+              case FacebookLoginStatus.loggedIn:
+                // print(result.)
+                print(result.accessToken.token);
+                break;
+              case FacebookLoginStatus.cancelledByUser:
+                throw new StateError("Cancelled by user");
+                break;
+              case FacebookLoginStatus.error:
+                throw new StateError(FacebookLoginStatus.error.toString());
+                break;
+            }
+          }) 
+          .catchError((e) {
+            print("ERROR");
+            print(e);
+          });
+          // Navigator.pushNamed(context, "/home");
         },
         backgroundColor: backgr,
         label: Container(
@@ -58,28 +88,75 @@ class _LoginPage extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        _buildBackground(),
-        Center(
-          child: Container(
-            padding: EdgeInsets.fromLTRB(0, globals.mh(context) * 0.2, 0, 0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  child: Text("LOGIN & REGISTER",
-                      style: Theme.of(context).textTheme.title),
-                ),
-                SizedBox(
-                  height: 40,
-                ),
-                _floatingButton("Google"),
-              ],
-            ),
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          _buildBackground(),
+          Column(
+            children: <Widget>[
+              Container(
+                height: globals.mh(context) * 0.4,
+                child: Center(
+                  child: Image.asset(
+                    "assets/images/logo.png",
+                    height: 140),
+                )
+              ),
+              Container(
+                height: globals.mh(context) * 0.6,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      width: 300,
+                      padding: EdgeInsets.all(10),
+                      child: TextField(
+                        focusNode: focusNode,
+                        autofocus: true,
+                        controller: usernameController,
+                        decoration: InputDecoration(
+                          hintText: "Username",
+                          labelText: "Username",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20)
+                          )
+                        ),
+                      )
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 5)),
+                    Container(
+                      width: 300,
+                      padding: EdgeInsets.all(10),
+                      child: TextField(
+                        focusNode: focusNode,
+                        obscureText: true,
+                        controller: passwordController,
+                        decoration: InputDecoration(
+                          hintText: "Password",
+                          labelText: "Password",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20)
+                          )
+                        ),
+                      )
+                    ),
+                    Container(
+                      child: FlatButton(
+                        onPressed: () => Navigator.pushNamed(context, "/home"),
+                        child: Text("LOG IN"),
+                        color: Theme.of(context).primaryColor
+                      )
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    _floatingButton("Facebook")
+                  ],
+                )
+              ),
+            ],
           ),
-        )
-      ],
+        ],
+      ),
     );
   }
 }
