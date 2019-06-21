098eb691949867fc77de5eaa9e2ed977469b1b09
joe43 <joe43@gitlab.com>
2019-06-21 16:42:56 +0700

    rev bug

--- lib/services/bid_services.dart
+++ lib/services/bid_services.dart
@@ -7,11 +7,14 @@ import 'package:jlf_mobile/models/bid.dart';
 Future<bool> placeBid(String token, Bid _data) async {
   final header = {"Content-Type": "application/json", "Authorization": token};
   print(json.encode(_data));
-  http.Response res = await http.post(getBaseUrl() + "/bids",
-      headers: header, body: json.encode(_data)).timeout(Duration(seconds: getTimeOut()));
-  
+  http.Response res = await http
+      .post(getBaseUrl() + "/bids", headers: header, body: json.encode(_data))
+      .timeout(Duration(seconds: getTimeOut()));
+
   if (res.statusCode == 201) {
     return true;
+  } else if (res.statusCode == 406) {
+    return false;
   } else {
     throw Exception(res.body);
   }
