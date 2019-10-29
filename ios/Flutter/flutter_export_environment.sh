87753b28eb6c673cbe926a40f173c48fba1cbc06
wanfintara <wanfintara@gmail.com>
2019-10-29 07:10:51 +0700

    iOS: change to realease configuration

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -2,9 +2,9 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/wanfintara/sdk/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/wanfintara/Documents/flutter/jlf2"
-export "FLUTTER_TARGET=/Users/wanfintara/Documents/flutter/jlf2/lib/main_development.dart"
+export "FLUTTER_TARGET=lib/main_production.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/wanfintara/sdk/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/wanfintara/sdk/flutter/bin/cache/artifacts/engine/ios-release"
 export "FLUTTER_BUILD_NAME=0.1.8"
 export "FLUTTER_BUILD_NUMBER=0.1.8"
